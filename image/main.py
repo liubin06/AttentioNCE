dab8cfd0789ebfff79496ae7bdf68889a6283a87
Liu Bin <269336620@qq.com>
2024-07-13 17:16:18 +0800

    Update main.py

--- image/main.py
+++ image/main.py
@@ -103,8 +103,8 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', default=400, type=int, help='Number of sweeps over the dataset to train')
     parser.add_argument('--estimator', default='SimCLR', type=str, help='SimCLR Framework')
     parser.add_argument('--dataset_name', default='cifar10', type=str, help='Choose dataset')
-    parser.add_argument('--d_pos', default=1, type=float, help='Number of images in each mini-batch')
-    parser.add_argument('--d_neg', default=1, type=float, help='Number of images in each mini-batch')
+    parser.add_argument('--d_pos', default=1, type=float, help='Scaling factor for positive samples')
+    parser.add_argument('--d_neg', default=1, type=float, help='Scaling factor for negative samples')
 
 
     # args parse
